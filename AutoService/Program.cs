7e84f24afb8b3a088b3752674b869dd2c8eca3cc
ruvim-ebs <ruvim.stratila@outlook.com>
2024-10-28 16:11:39 +0200

    registred all dependecies in IoC

--- AutoService/Program.cs
+++ AutoService/Program.cs
@@ -1,21 +1,67 @@
+using Application.Contracts.Commands.AppointmentCommands.Create;
+using Application.Contracts.Commands.CarCommands.Create;
+using Application.Contracts.Commands.CarCommands.Update;
+using Application.Contracts.Commands.DayScheduleCommands.Update;
+using Application.Contracts.Commands.ServiceCommands.Create;
+using Application.Contracts.Commands.UserCommands.Create;
+using Application.DTOs.Appointment;
+using Application.DTOs.Car;
+using Application.DTOs.CarDtos;
+using Application.DTOs.DaySchedule;
+using Application.DTOs.Service;
+using Application.DTOs.UserDtos;
+using Application.Validators.AppointmentValidators;
+using Application.Validators.CarValidatoros;
+using Application.Validators.DaySchedulevalidators;
+using Application.Validators.ServiceValidators;
+using Application.Validators.UserValidators;
+using Domain.DomainServices.AppointmentService;
+using Domain.IRepositories;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Mapster;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence.DBContext;
+using Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+//---BD
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add services to the container.
+//---Repositories
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ICarRepository, CarRepository>();
+builder.Services.AddScoped<ISlotRepository, SlotRepository>();
+builder.Services.AddScoped<IServiceRepository, ServiceRepository>();
+builder.Services.AddScoped<IDayScheduleRepository, DayScheduleRepository>();
+builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+
+//---Service
+builder.Services.AddScoped<IAppointmentService, AppointmentService>();
+
+//---MediatR
+builder.Services.AddMediatR(typeof(CreateCarCommandHandler).Assembly);
+
+//---Validators
+
+builder.Services.AddFluentValidation(fv => 
+{
+    fv.RegisterValidatorsFromAssemblyContaining<CreateCarDtoValidator>();
+});
+
+//---Mapper-ul
+builder.Services.AddMapster();
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
