41dd0134b71abc817a3f9fe84860ff8971c92e87
Stratila Ruvim <ruvim.stratila@gmail.com>
2024-10-29 17:50:18 +0200

    created DayScheduleController, ServiceController

---
+++ AutoService/Controllers/DaySchedule/DayScheduleController.cs
@@ -0,0 +1,53 @@
+using Application.Contracts.Commands.DayScheduleCommands.Update;
+using Application.Contracts.Queries.DayScheduleQueries.Get;
+using Application.Contracts.Queries.DayScheduleQueries.GetAll;
+using Application.Contracts.Queries.DayScheduleQueries.GetByDayOfWeek;
+using Application.DTOs.DaySchedule;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoService.Controllers.DaySchedule;
+
+[ApiController]
+[Route("[controller]")]
+public class DayScheduleController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public DayScheduleController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Get([FromRoute]int id)
+    {
+        var result = await _mediator.Send(new GetDayScheduleQuery(id));
+        if(result.IsFailed) return BadRequest(result.Errors);
+        return Ok(result.Value);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await _mediator.Send(new GetAllDayScheduleQuery());
+        if(result.IsFailed) return BadRequest(result.Errors);
+        return Ok(result.Value);
+    }
+
+    [HttpGet("[action]")]
+    public async Task<IActionResult> GetByDay([FromBody] DayOfWeek dayOfWeek)
+    {
+        var result = await _mediator.Send(new GetByDayOfWeekQuery(dayOfWeek));
+        if(result.IsFailed) return BadRequest(result.Errors);
+        return Ok(result.Value);
+    }
+
+    [HttpPut("[action]")]
+    public async Task<IActionResult> Update([FromBody] UpdateDayScheduleDto updateDto)
+    {
+        var result = await _mediator.Send(new UpdateDayScheduleCommand(updateDto));
+        if(result.IsFailed) return BadRequest(result.Errors);
+        return Ok(result.Value);
+    }
+}
---
+++ AutoService/Controllers/Service/ServiceController.cs
@@ -0,0 +1,63 @@
+using Application.Contracts.Commands.ServiceCommands.Create;
+using Application.Contracts.Queries.ServiceQueries.Get;
+using Application.Contracts.Queries.ServiceQueries.GetAll;
+using Application.Contracts.Queries.ServiceQueries.GetByMaster;
+using Application.Contracts.Queries.ServiceQueries.GetByType;
+using Application.DTOs.Service;
+using Domain.Enums;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoService.Controllers.Service;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ServiceController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ServiceController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get([FromRoute] int id)
+    {
+        var result = await _mediator.Send(new GetServiceQuery(id));
+        if(result.IsFailed) return BadRequest(result.Errors);
+        return Ok(result.Value);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await _mediator.Send(new GetAllServiceQuery());
+        if (result.IsFailed) return BadRequest(result.Errors);
+        return Ok(result.Value);
+    }
+
+    [HttpGet("[action]/{id}")]
+    public async Task<IActionResult> GetMastersServices([FromRoute] int id)
+    {
+        var result = await _mediator.Send(new GetServiceByMasterQuery(id));
+        if (result.IsFailed) return BadRequest(result.Errors);
+        return Ok(result.Value);
+    }
+
+    [HttpGet("[action]")]
+    public async Task<IActionResult> GetServicesByType([FromBody] ServiceType serviceType) // [FromQuery] ???
+    {
+        var result = await _mediator.Send(new GetServiceByTypeQuery(serviceType));
+        if (result.IsFailed) return BadRequest(result.Errors);
+        return Ok(result.Value);
+    }
+
+    [HttpPost("[action]")]
+    public async Task<IActionResult> Create([FromBody] CreateServiceDto createService)
+    {
+        var result = await _mediator.Send(new CreateServiceCommand(createService));
+        if (result.IsFailed) return BadRequest(result.Errors);
+        return Ok(result.Value);
+    }
+}
